6114c6ac5d3531d69bf87e4282a6b684b5b86beb
Shivamt7 <shivam.s.mishra7@gmail.com>
2025-07-14 23:34:38 +0530

    Project Update

--- src/components/work/WorkItems.jsx
+++ src/components/work/WorkItems.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const WorkItems = ({item}) => {
   return (
     <div className="work__card" key={item.id}>
-        <img src={item.image} alt={item.title} width='300px'/>
+        <img className='work__image' src={item.image} alt={item.title}/>
         <h3 className="work__title">{item.title}</h3>
         <a href={item.url} className="work__button">Website
             <i className="bx bx-right-arrow-alt work__button-icon"></i>
